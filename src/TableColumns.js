15699f107f3b690a13976b56895196dae58a710d
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2024-03-28 21:43:46 +0100

    second commit

--- src/TableColumns.js
+++ src/TableColumns.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TableColumns = ( {value} ) => {
   return (
-    <td key={value.value}>{`${value.key}:` + JSON.stringify(value.value)}</td>
+    <td key={value.value}>{`${value.key}:` + JSON.stringify(value.value)}</td> // I am concatenating here
   )
 }
 
